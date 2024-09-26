f695809432cf7c019c14354bfb4d6de9dc453076
Franck Yin <franckyin@hotmail.com>
2024-09-26 10:21:59 -0400

    Bidirectional Core References

--- Main.ps1
+++ Main.ps1
@@ -63,7 +63,7 @@ function Main {
                         # With audit properties of either:
                         # On the Join side of a Many to Many relationship
                         # On the Child side of a Parent-Child relationship
-                        if ($_.Decorator -eq "PK" -or $_.AuditProp -eq "J - J" -or $_.AuditProp -eq "C") {
+                        if ($_.Decorator -eq "PK" -or $_.AuditProp -in @("N - J", "J - J", "P", "C")) {
                             $auditTableInfo.Group += @(Generate-AuditField($_))
                             
                             # Demoting orignal PK field to FK
