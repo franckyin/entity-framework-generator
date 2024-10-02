b7cac21e40c5d3542da4351f9187582dee73c5ea
Franck Yin <franckyin@hotmail.com>
2024-10-02 15:35:11 -0400

    ForeignKey decorator management

--- ps-functions/Generate-FieldDecorator.ps1
+++ ps-functions/Generate-FieldDecorator.ps1
@@ -8,8 +8,14 @@ function Generate-FieldDecorator {
     $fieldDecorator = ""
 
     # Decorators are only required in the Entity class definition
-    if ($dataClassType -eq "Entity" -and $fieldInfo.Decorator.Contains("StringLength")) {
-        $fieldDecorator = $fieldInfo.Decorator
+    if ($dataClassType -eq "Entity") {
+        if ($fieldInfo.Decorator.Contains("StringLength")) {
+            $fieldDecorator = $fieldInfo.Decorator
+        }
+        elseif ($fieldInfo.Decorator -eq "NP") {
+            $fkId = $fieldInfo.FieldCodeName -replace "Ref$", "Id"
+            $fieldDecorator = '[ForeignKey("'+$fkId+'")]'
+        }
     }
 
     if ($fieldDecorator -ne "") {
